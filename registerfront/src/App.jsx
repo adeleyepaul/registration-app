f834bfe47d9ac0cfc5d5e8001892a95152179a84
adeleyepaul <adeleyepaul>
2022-11-02 10:36:22 +0100

    second commit

---
+++ registerfront/src/App.jsx
@@ -0,0 +1,89 @@
+import React, {Component} from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            fullName:'',
+            username:'',
+            email:'',
+            password:''
+        }
+        this.changeFullName = this.changeFullName.bind(this)
+        this.changeUsername = this.changeUsername.bind(this)
+        this.changeEmail = this.changeEmail.bind(this)
+        this.changePassword = this.changePassword.bind(this)
+    }
+
+    changeFullName(event){
+        this.setState({
+            fullName:event.target.value
+        })
+    }
+
+    changeUsername(event){
+        this.setState({
+            Username:event.taget.value
+        })
+    }
+
+    changeEmail(event){
+        this.setState({
+            email:event.target.value
+        })
+    }
+
+    changePassword(event){
+        this.setState({
+            password:event.target.value
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <div className="container">
+                    <div className="form-div">
+                        <form>
+                            <input type="text"
+                            placeholder="Full Name"
+                            onChange={this.changeFullName}
+                            value={this.state.fullName}
+                            className='form-control form-group'
+                            />
+
+                            <input type="text"
+                            placeholder="username"
+                            onChange={this.changeUsername}
+                            value={this.state.Username}
+                            className='form-control form-group'
+                            />
+
+                            <input type="text"
+                            placeholder="email"
+                            onChange={this.changeEmail}
+                            value={this.state.email}
+                            className='form-control form-group'
+                            />
+
+                            <input type="text"
+                            placeholder="password"
+                            onChange={this.changePassword}
+                            value={this.state.password}
+                            className='form-control form-group'
+                            />
+
+                            <input type="submit"
+                            className="btn btn-danger btn-block"
+                            value="submit"
+                            />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
